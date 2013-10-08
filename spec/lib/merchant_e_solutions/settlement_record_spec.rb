8d30216a8b5626c6c87a6ff92f6a5b05e24d51db
Steve Ellis <email@steveell.is>
2013-10-08 16:16:05 -0400

    Rename spec file to follow convention

---
+++ spec/lib/merchant_e_solutions/settlement_record_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe MerchantESolutions::SettlementRecord do
+  describe "#initialize" do
+    subject { MerchantESolutions::SettlementRecord.new(csv_row) }
+    # "Org Id","Org Name","Card Type","Sales Cnt","Sales Amt","Credits Cnt","Credits Amt","Net Amt"
+    let(:csv_row) { ['42', "Hamazon", "Visa", 1337, 1000000000.0, 1, 0.01, 999999999.99] }
+
+    its(:organization_id) { should == '42' }
+    its(:organization_name) { should == "Hamazon" }
+    its(:card_type) { should == "Visa" }
+    its(:sales_count) { should == 1337 }
+    its(:sales_amount) { should == 1000000000.0 }
+    its(:credits_count) { should == 1 }
+    its(:credits_amount) { should == 0.01 }
+    its(:net_amount) { should == 999999999.99 }
+  end
+end
